dbd1d6bd39bda98caa0532a9dcd73cafeaf448f1
zp <173343021@qq.com>
2020-04-16 18:33:13 +0800

    416_2

---
+++ src/main/java/algorithm/IsPalindrome.java
@@ -0,0 +1,43 @@
+package algorithm;
+
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    private static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        char[] array = Integer.toString(x).toCharArray();
+        int len = array.length;
+        int i = 0;
+        int y = len - 1;
+
+        while (i <= y) {
+            if (array[i] != array[y]) {
+                return false;
+            }
+            i++;
+            y--;
+        }
+        return true;
+
+    }
+
+    private static boolean isPalindrome2(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int pre = 0;
+        while (x > pre) {
+            pre = pre * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return x == pre || x == pre / 10;
+    }
+}
+
